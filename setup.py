2a5d13a3158d70e5ba680f5699e96e65609e1da1
Vladimir Kozhukalov <kozhukalov@gmail.com>
2021-09-29 20:19:00 +0300

    Initial commit of taskmanage

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+from setuptools import find_packages
+
+
+setuptools.setup(
+    name="taskmanage",
+    version="0.0.1",
+    packages=find_packages(),
+    description="taskmanage",
+    author="Dubna IES",
+    classifiers=["Intended Audience :: Information Technology",
+                 "Operating System :: POSIX :: Linux",
+                 "Programming Language :: Python",
+                 "Programming Language :: Python :: 3",
+                 ]
+)
