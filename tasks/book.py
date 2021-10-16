b07cf7f8ca33b245e94a054fcc4faf72914a67cb
Andrewsha2002 <krasnykov2018@gmail.com>
2021-10-16 15:37:13 +0400

    add funk

--- tasks/book.py
+++ tasks/book.py
@@ -5,7 +5,9 @@ import time
 link = 'https://www.gutenberg.org/files/2554/2554-0.txt'
 file = "Crime_and_Punishment.txt"
 
-with requests.get(link, stream=True) as response:
-    with open(file, "wb") as c:
-        for chunk in response.iter_content(chunk_size=8192):
-            c.write(chunk)
+def book():
+    with requests.get(link, stream=True) as response:
+        with open(file, "wb") as c:
+            for chunk in response.iter_content(chunk_size=8192):
+                c.write(chunk)
+
