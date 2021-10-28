45d40762ac90caabaf4ca5abd308be4be57eb035
Andrewsha2002 <krasnykov2018@gmail.com>
2021-10-28 18:44:35 +0400

    добавлены таски. операции переведены в функции

--- tasks/download.py
+++ tasks/download.py
@@ -1,20 +1,36 @@
 import yaml
-
-from pprint import pprint
+from taskmanage.task import Task
 import requests
+from functools import partial
 
-with open("books.yaml" , "r") as c:
+with open("books.yaml") as c:
     data = yaml.safe_load(c)
 
-pprint(data)
 
-for item in data["books"]:
-    path = "books/" + item["filename"]
+def download(bookpath):
+    for item in data["books"]:
+        path = "books/" + item["filename"]
+        with requests.get(item["link"], stream=True) as response:
+            with open(path, "wb")  as c:
+                for chunk in response.iter_content(chunk_size=8192):
+                    c.write(chunk)
 
 
+def create_tasks():
 
 
-    with requests.get(item["link"], stream=True) as response:
-        with open(path, "wb")  as c:
-            for chunk in response.iter_content(chunk_size=8192):
-                 c.write(chunk)
+    tasks = []
+
+    for item in data["books"]:
+        path = "books/" + item["filename"]
+        task = Task(path)
+        funk_without_arguments = partial(download, path)
+        task.set_action(funk_without_arguments)
+
+        tasks.append(task)
+
+    return tasks
+
+for t in create_tasks():
+    t.run()
+
