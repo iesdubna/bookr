0d752c68f7b35d91e6c1dcc3ced414d4f13842f4
alex-py231 <kuznaa.20@uni-dubna.ru>
2021-11-01 15:39:41 +0300

    HW_4_3

--- tasks/sample.py
+++ tasks/sample.py
@@ -1,84 +1,80 @@
 import time
+import nltk
 from taskmanage import task
 import requests
-from bs4 import BeautifulSoup
-from bs4 import BeautifulSoup
-import string 
+from nltk.tokenize import sent_tokenize
+import yaml
+import numpy
+from functools import partial
 
-def fake_action_0():
-    with requests.get('https://www.gutenberg.org/cache/epub/910/pg910-images.html',stream=True) as res:
-        f=open("book.txt","wb")
-        for chunk in res.iter_content(chunk_size=8192):
-            f.write(chunk)
-        f.close()
+def analysis(length):
+    with open("books.yaml","r") as f:
+        data=yaml.safe_load(f)
+    for book in data["download"]:
+        with requests.get(book["link"],stream=True) as res:
+            f=open(book["filename"],"wb")
+            for chunk in res.iter_content(chunk_size=8192):
+                f.write(chunk)
+            f.close()
+        len_sent=[]
+        with open(book["filename"],"r",encoding="UTF-8") as f:
+            for st in f:
+                tmp=sent_tokenize(st)
+                for i in tmp:   
+                    len_sent.append(len(i.split()))
+        sum=0
+        for i in len_sent:
+            if length==i:
+                sum+=1
+        num_sent=sum
+        str_max = max(len_sent)
+        str_min = min(len_sent)
+        sum=0
+        for i in len_sent:
+            sum+=i
+        str_mid = int(sum/len(len_sent))
+        b=[]
+        with open(book["filename"],"r",encoding="UTF-8") as f:
+            for line in f:
+                nltk_tokens = nltk.word_tokenize(line)
+                b.append((list(nltk.bigrams(nltk_tokens))))
+        a=[]
+        for i in b:
+            for j in list(i):
+                b=j[0]+ " "+ j[1]
+                a.append(b)
+        for i in a:
+            i = i.translate({ord(j): None for j in '.'})
+            i = i.translate({ord(j): None for j in '!'})
+            i = i.translate({ord(j): None for j in '?'})
+            i = i.translate({ord(j): None for j in ','})
+            i = i.translate({ord(j): None for j in '-'})
+        for i in a:
+            if len(i.split())==1:
+                a.remove(i)
+            else:
+                pass
+        cleared_bigrams = list(dict.fromkeys(a))
+        sum = 0
+        arr = []
+        for i in cleared_bigrams:
+            for j in a:
+                if i == j:
+                    sum += 1
+            arr.append(sum)
+            sum = 0
+        resul=""
+        for i in range(20):
+            res = numpy.argmax(arr)
+            resul+=(cleared_bigrams[res])+"\n"
+            del arr[res]
+        name_book=str(book["filename"])
+        result={"Length of the longest sentence": str(str_max), "Average sentence length" : str(str_mid) , "The length of the shortest sentence": str(str_min),"Number of length sentences "+ str(length) :str(num_sent),"The 20 most frequent phrases:":resul}
+        with open("sentence_analysis_"+ name_book+".yaml","w") as f:
+            for key, value in result.items():
+                f.write(f'{key}, {value}\n')
 
-def fake_action_1():
-    mas_text=[]
-    f=open("book.txt","rb")
-    mas_byte=f.readlines()
-    for i in range(len(mas_byte)):
-        soup = BeautifulSoup(mas_byte[i], 'html.parser')
-        mas_text.append(soup.get_text())
-    f.close()
-    f=open("book.txt","w",encoding="utf-8")
-    for i in range(len(mas_text)):
-        f.write(mas_text[i])
-    f.close()
-
-def fake_action_2():
-    mass_text=[]
-    f=open("book.txt","r",encoding="utf-8")
-    text=f.readlines()
-    for i in range(115,1264):
-        mass_text.append(text[i])
-    for i in range(len(mass_text)):
-        for p in string.punctuation:
-            if p in mass_text[i]:
-                mass_text[i] = mass_text[i].replace(p, '')         
-    mas_text=['.']
-    for line in mass_text:
-        if (len(line.strip())):
-            mas_text.append([line]) 
-    mas_word_2 = []
-    for i in range(len(mas_text)):
-        mas_word_2.append([y for y in str(mas_text[i]).lower().split()])
-    mas_word_1=sum(mas_word_2, [])
-    mas_u_word=[]
-    for item in mas_word_1: 
-        if item not in mas_u_word: 
-            mas_u_word.append(item)
-    res=[]
-    for i in range(len(mas_u_word)):
-        res.append(int(0))
-    for i in range(len(mas_u_word)):
-        for j in mas_word_1:
-            if j==mas_u_word[i]:
-                res[i]+=1
-    for i in range(10):
-        print(mas_u_word[res.index(max(res))])
-        del mas_u_word[res.index(max(res))]
-    print('----')
-    print('----')
-    print('----')
-    print('----')
-    print('----')
-    for i in range(10):
-        print(mas_u_word[res.index(min(res))])
-        del mas_u_word[res.index(min(res))]
-
+funk_without_arguments = partial(analysis,5)
 task0 = task.Task("0")
-task0.set_action(fake_action_0)
-
-task1 = task.Task("1")
-task1.set_action(fake_action_1)
-
-task2 = task.Task("2")
-task2.set_action(fake_action_2)
-task1.depends_on(task0)
-task2.depends_on(task0)
-taskgraph = task.Taskgraph()
-
-taskgraph.append_task(task0)
-taskgraph.append_task(task1)
-taskgraph.append_task(task2)
-taskgraph.run_correct_order()
+task0.action=funk_without_arguments
+task0.run()
