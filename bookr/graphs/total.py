a2f0cc76cf534b98d64f76313209ed5446df933f
Vladimir Kozhukalov <kozhukalov@gmail.com>
2021-11-28 12:16:54 +0300

    Refactor total graph

--- bookr/graphs/total.py
+++ bookr/graphs/total.py
@@ -3,20 +3,18 @@ from bookr.tasks import download
 from bookr.tasks import word_analysis
 from bookr.tasks import sentence_analysis
 
+
 def create_taskgraph():
     graph = taskgraph.Taskgraph()
     download_tasks = download.create_tasks()
     word_analysis_tasks = word_analysis.create_tasks()
     sentence_analysis_tasks = sentence_analysis.create_tasks()
     
-    for i in range(0,len(download_tasks)):
-        task0=download_tasks[i]
-        task1=word_analysis_tasks[i]
-        task2=sentence_analysis_tasks[i]
-        task1.depends_on(task0)
-        task2.depends_on(task0)
+    for i in range(0, len(download_tasks)):
+        word_analysis_tasks[i].depends_on(download_tasks[i])
+        sentence_analysis_tasks[i].depends_on(download_tasks[i])
 
-        graph.append_task(task1)
-        graph.append_task(task2)
-        graph.append_task(task0)
+        graph.append_task(download_tasks[i])
+        graph.append_task(word_analysis_tasks[i])
+        graph.append_task(sentence_analysis_tasks[i])
     return graph
